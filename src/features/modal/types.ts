3167dff19c1d37a782e3fa87076ec4b0b0b2c37f
isupov <t.1113@ya.ru>
2025-07-24 17:12:43 +0300

    Added possibility to delete operation item after submit in popup

--- src/features/modal/types.ts
+++ src/features/modal/types.ts
@@ -4,9 +4,10 @@ export const MODAL_NAME = {
     MOBILE_MENU: 'mobileMenu',
     FINANCE_FORM: 'financeForm',
     CATEGORY_FIELD: 'categoryField',
+    DELETE_OPERATION: "deleteOperation",
 } as const;
 
-export type ModalName = (typeof MODAL_NAME)[keyof typeof MODAL_NAME];
+export type ModalName = (typeof MODAL_NAME)[keyof typeof MODAL_NAME] | string;
 
 export interface ModalItem {
     name: ModalName;
--- src/features/operations/components/OperationFields/OperationDeleteField.tsx
+++ src/features/operations/components/OperationFields/OperationDeleteField.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import { AllOperations } from "../../types";
+import { Modal } from "../../../modal/components/Modal";
+import { useModal } from "../../../modal/hooks/useModal";
+import { MODAL_NAME } from "../../../modal/types";
+
+const getOperationModalId = ( id: string ) =>
+    `${ MODAL_NAME.DELETE_OPERATION }_${ id }`;
 
 interface OperationDeleteFieldProps<T extends AllOperations> {
     item: T,
@@ -13,8 +19,35 @@ export const OperationDeleteField =
             onRemove
         }: OperationDeleteFieldProps<T>
     ) => {
+        const { open, close, isOpen } = useModal();
+        const modalId = getOperationModalId( item.id );
+
         return (
-            <button onClick={ () => onRemove( item ) }>Удалить</button>
+            <>
+                <button
+                    onClick={ () => open( modalId ) }>
+                    Удалить
+                </button>
+
+                <Modal isOpen={ isOpen( modalId ) }
+                       onClose={ () => close( modalId ) }>
+                    <>
+                        Удалить?
+                        <button
+                            onClick={ () => {
+                                onRemove( item );
+                                close( modalId )
+                            } }>
+                            Да
+                        </button>
+                        <button
+                            onClick={ () => close( modalId ) }>
+                            Нет
+                        </button>
+                    </>
+
+                </Modal>
+            </>
         );
     }
 
