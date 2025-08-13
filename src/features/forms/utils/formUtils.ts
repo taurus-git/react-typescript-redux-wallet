574c0ac3c27f21945e216bb7c1466346899fb772
isupov-zfort <86831940+isupov-zfort@users.noreply.github.com>
2025-08-13 16:56:15 +0300

    Changed formatDate, as previous version shows hours not in current locale

--- src/features/forms/utils/formUtils.ts
+++ src/features/forms/utils/formUtils.ts
@@ -35,5 +35,11 @@ export const getTransferFormFields = ( formData: FormData ): Omit<Transfer, "id"
 });
 
 export const formatDate = ( date: Date ) => {
-    return date.toISOString().slice( 0, 16 );
+    const year = date.getFullYear();
+    const month = String( date.getMonth() + 1 ).padStart( 2, '0' );
+    const day = String( date.getDate() ).padStart( 2, '0' );
+    const hours = String( date.getHours() ).padStart( 2, '0' );
+    const minutes = String( date.getMinutes() ).padStart( 2, '0' );
+
+    return `${ year }-${ month }-${ day }T${ hours }:${ minutes }`;
 }
