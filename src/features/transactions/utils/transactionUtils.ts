0ba30522fe32948db0bf8b44cc7d2492a57f293a
isupov-zfort <86831940+isupov-zfort@users.noreply.github.com>
2025-08-14 17:27:43 +0300

    Simplified isTransaction function

--- src/features/transactions/utils/transactionUtils.ts
+++ src/features/transactions/utils/transactionUtils.ts
@@ -2,10 +2,9 @@ import { TransactionType } from "../types";
 import { Transaction } from "../types";
 import { MoneyOperation } from "../../operations/types";
 
-//Todo: simplify this
 export const isTransaction = ( type: MoneyOperation ): type is Transaction => {
-    return (
-        type.transactionType === TransactionType.EXPENSE ||
-        type.transactionType === TransactionType.INCOME
-    )
+    return [
+        TransactionType.EXPENSE,
+        TransactionType.INCOME
+    ].includes( type.transactionType );
 }
